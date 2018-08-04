6d0ddf3260acd33ba07d94411aa2de5613618b66
Rosie rose rose <rvl24@cornell.edu>
2018-08-04 13:14:00 -0400

    started work on fixtures

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -1,18 +1,6 @@
-from onshapepy.part import Part
-from onshapepy.assembly import Assembly
 from onshapepy.client import Client
 
-# load the client for this part
-my_cube = Part("2d47b6abec9d1de1d2538372", "f2d396ba0762dc1f1dab3de1", "0639ea3c439aa0947744d29a")
-my_pyramid = Assembly("8ec353ba00f37f447b5a61f5", "04c36c786829759832bd3d1a", "1a1dbfb527f96cc00c05e2ed")
-c = Client()
-
-def test_insert():
-
-    # set various parameters
-    my_cube.params["h"] = {}
-    my_cube.params["h"]["value"] = 5 * 20
-    my_cube.params["h"]["units"] = "inch"
+def test_insert(cube, assembly):
 
     # Insert into the assembly
-    my_pyramid.insert(my_cube, c)
+    assembly.insert(cube, Client())
