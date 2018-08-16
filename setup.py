7b118f1f4677cf1c4ef6ede9e4eeff1e1a808031
ethan92429 <ethan.keller@gmail.com>
2018-08-16 12:53:38 -0400

    update setup so that it finds core package

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 long_description = '''Access OnShape part configurations from your python interpreter! Allows you to use Numpy, Scipy, and any other python packages you may need to accurately configure your part. An open source alternative to CADWolf or SwiftCalcs.'''
 
 setup(
     name= 'onshapepy',
-    version= '0.0.6',
+    version= '0.0.7',
     description= 'Drive part configurations from Python.',
     long_description= long_description,
     author= 'Ethan Keller',
     url= 'https://aguaclara.github.io/onshapepy/',
     license= "The MIT License (MIT)",
-    packages= [
-        'onshapepy'
-    ],
+    packages=find_packages(),
     install_requires= ['requests', 'pint', 'ruamel.yaml'],
 )
 
